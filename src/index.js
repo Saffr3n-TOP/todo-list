adcb39abdf50a9fb7a861cba50d0462a209b3379
Saffr3n <saffr3n@gmail.com>
2023-11-04 23:49:00 +0300

    Initialize localStorage on load

--- src/index.js
+++ src/index.js
@@ -1,8 +1,12 @@
 // @ts-check
+import DB from './db';
+
 import '../assets/styles/style.css';
 import Header from './components/header';
 import Nav from './components/nav';
 import Main from './components/main';
+
+DB.initialize();
 
 const root = document.querySelector('.root');
 root?.append(Header(), Nav(), Main());
