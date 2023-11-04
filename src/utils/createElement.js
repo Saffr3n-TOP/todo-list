995b3942f1ff58fc454bffd9d0d39c05769af949
Saffr3n <saffr3n@gmail.com>
2023-11-04 23:57:25 +0300

    Expand createElement utility's functionality
    - Add form, label, input, textarea, select, option tags support
    - Add id attribute support
    - Add role attribute for upcoming dialogs
    - Expand type attribute possible values to support inputs
    - Also add value and required attributes support for inputs
    - Add htmlFor attribute support for labels
    - Add method attribute support for forms
    - Add onkeydown and onsubmit events support

--- src/utils/createElement.js
+++ src/utils/createElement.js
@@ -1,17 +1,26 @@
 // @ts-check
 
 /**
- * @param {'header' | 'nav' | 'main' | 'div' | 'span' | 'h1' | 'h2' | 'p' | 'a' | 'button' | 'img' | 'ul' | 'li'} tagName
+ * @param {'header' | 'nav' | 'main' | 'div' | 'span' | 'h1' | 'h2' | 'p' | 'a' | 'button' | 'img' | 'ul' | 'li' | 'form' | 'label' | 'input' | 'textarea' | 'select' | 'option'} tagName
  * @param {{
  *   children?: HTMLElement[],
  *   attributes?: { key: string, value: string }[],
+ *   id?: string,
  *   className?: string,
  *   textContent?: string,
- *   type?: 'button' | 'submit',
+ *   role?: 'dialog',
+ *   type?: 'button' | 'submit' | 'text' | 'date' | 'checkbox',
+ *   name?: string,
+ *   value?: string,
+ *   required?: boolean,
+ *   htmlFor?: string,
  *   href?: string,
  *   alt?: string,
  *   src?: string,
- *   onclick?: (e: MouseEvent) => void
+ *   method?: 'POST' | 'PUT' | 'DELETE',
+ *   onclick?: (e: MouseEvent) => void,
+ *   onkeydown?: (e: KeyboardEvent) => void,
+ *   onsubmit?: (e: SubmitEvent) => void
  * }} opts
  */
 export default function createElement(tagName, opts) {
